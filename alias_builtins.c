9c9eb261088505e522129eb9c65ee622276aad59
NRIAMA CHIBUZO MARYDONALD <105368562+marydonald@users.noreply.github.com>
2022-08-05 11:38:19 -0700

    Update alias_builtins.c

--- alias_builtins.c
+++ alias_builtins.c
@@ -8,7 +8,7 @@ void print_alias(alias_t *alias);
  * shellby_alias - Builtin command that either prints all aliases, specific
  * aliases, or sets an alias.
  * @args: An array of arguments.
- * @front: A double pointer to the beginning of args
+ * @front: A double pointer to the beginning of args.
  *
  * Return: If an error occurs - -1.
  *         Otherwise - 0.
