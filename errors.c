0f32f6ea6f98eab3ec210f895868c4475a867fa1
NRIAMA CHIBUZO MARYDONALD <105368562+marydonald@users.noreply.github.com>
2022-08-05 11:46:23 -0700

    Update errors.c

--- errors.c
+++ errors.c
@@ -5,7 +5,7 @@ char *_itoa(int num);
 int create_error(char **args, int err);
 
 /**
- * num_len - Counts the digit length of a number.
+ * num_len - Counts the digit length of a number
  * @num: The number to measure.
  *
  * Return: The digit length.
