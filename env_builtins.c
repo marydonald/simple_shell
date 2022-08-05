b51643754d80577900226be76544cda00b7009e5
NRIAMA CHIBUZO MARYDONALD <105368562+marydonald@users.noreply.github.com>
2022-08-05 11:42:15 -0700

    Update env_builtins.c

--- env_builtins.c
+++ env_builtins.c
@@ -5,9 +5,9 @@ int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
 int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
 
 /**
- * shellby_env - Prints the current environment.
+ * shellby_env - Prints the current environment
  * @args: An array of arguments passed to the shell
- * @front: A double pointer to the beginning of args
+ * @front: A double pointer to the beginning of args.
  *
  * Return: If an error occurs - -1.
  *	   Otherwise - 0.
